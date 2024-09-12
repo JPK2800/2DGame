98b4dcb8c2caab9f1909dfca5b7172f0f30c6d1c
JPK <toximail2@gmail.com>
2024-09-12 10:21:56 -0400

    initial push & base game

---
+++ Source/Strat/Public/GameAiController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "GameAiController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRAT_API AGameAiController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
